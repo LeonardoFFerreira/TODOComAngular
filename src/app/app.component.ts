dde2c7117b06e7f597c5c6bf7ad8e968c5675134
LeonardoFFerreira <devleofreitas@gmail.com>
2021-11-02 16:34:54 -0300

    Removendo um item da lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,16 @@ export class AppComponent {
   public title: string = 'Minhas tarefas';
 
   constructor() {
-    this.todos.push(new Todo(1,'Passear com o dog', false));
-    this.todos.push(new Todo(2,'Ir ao mercado', false));
-    this.todos.push(new Todo(3,'Cortar o cabelo', true));
+    this.todos.push(new Todo(1, 'Passear com o dog', false));
+    this.todos.push(new Todo(2, 'Ir ao mercado', false));
+    this.todos.push(new Todo(3, 'Cortar o cabelo', true));
   }
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+  markAsDone() { }
+  markAsUndone() { }
 }
